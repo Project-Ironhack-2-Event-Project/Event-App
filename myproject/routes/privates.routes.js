aad00db63030017b98628d244c76a64798f39198
Juan Lozano <jalozanolopez@gmail.com>
2023-06-12 18:20:54 +0200

    Edit event routed in privates.routes

--- myproject/routes/privates.routes.js
+++ myproject/routes/privates.routes.js
@@ -3,10 +3,38 @@ const router = express.Router();
 
 const Event = require('../models/Event.model');
 
-
+// EDIT EVENT
 router.get("/profil/:id/edit", (req, res, next) => {
-    res.render('privates/edit-event');
+    const eventId = req.params.id
+    // console.log(req.params);
+    Event.findById(eventId)
+        .then(eventToEdit => {
+            res.render('privates/edit-event', {
+                event: eventToEdit
+            });
+        })
+        .catch(e => next(e))
 })
+router.post('/profil/:id/edit', (req, res, next) => {
+    const eventId = req.params.id
+    // console.log(req.params);
+    const editEvent = {
+        title: req.body.title,
+        place: req.body.place,
+        date: req.body.date,
+        description: req.body.description,
+        pictures: req.body.pictures,
+        likes: req.body.likes,
+        price: req.body.price
+    }
+    Event.findByIdAndUpdate(eventId, editEvent, {new: true})
+        .then(() => {
+            res.redirect('/profil')
+            // it think this should redirect to the details of the event we just edited
+        })
+        .catch(e => next(e))
+})
+
 
 
 router.get("/profil", (req, res, next) => {
