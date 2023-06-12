52fda12c3097952f8d6e6b17eaaab1be39bebcf5
Dok <pierredok@outlook.fr>
2023-06-12 18:28:53 +0200

    Edit Form without css, connect Event list with MongoDB

--- myproject/routes/event.routes.js
+++ myproject/routes/event.routes.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const router = express.Router();
-
+const Event = require('../models/Event.model')
 const mongoose = require("mongoose");
 
 router.get("/event", (req, res, next) => {
-    res.render('event/event-list')
+
+    Event.find()
+        .then((eventFromDB) => {
+            const eventData = {
+                eventList: eventFromDB
+            }    
+            res.render('event/event-list', eventData)
+        })
+        .catch((error) => {
+            console.log("error with event From DB eventlist", error)
+            next(error);
+        })
+
 })
 
-router.get("/event/id", (req, res, next) => {
+router.get("/event/:id", (req, res, next) => {
     res.render('event/event-details');
 })
 
