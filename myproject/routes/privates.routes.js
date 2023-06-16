d11e17d1a9831ce08f85c375ec39fb32fb938b86
Dok <pierredok@outlook.fr>
2023-06-16 10:43:59 +0200

    FIx edit with Cloudinary information

--- myproject/routes/privates.routes.js
+++ myproject/routes/privates.routes.js
@@ -14,7 +14,7 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 const isOwner = require("../middleware/isOwner");
 
 // EDIT EVENT
-router.get("/profil/:id/edit", isLoggedIn, (req, res, next) => {
+router.get("/profil/:id/edit", isLoggedIn, fileUploader.single('pictures'), (req, res, next) => {
     const eventId = req.params.id
     // console.log(req.params);
     Event.findById(eventId)
@@ -28,6 +28,11 @@ router.get("/profil/:id/edit", isLoggedIn, (req, res, next) => {
 })
 router.post('/profil/:id/edit', fileUploader.single('pictures'), (req, res, next) => {
     const eventId = req.params.id
+    console.log(req.file);
+    
+    const userId = req.session.userId
+    const usernameOfAuthor = req.session.currentUser.username
+    
     // console.log(req.params);
     const editEvent = {
         title: req.body.title,
@@ -38,7 +43,10 @@ router.post('/profil/:id/edit', fileUploader.single('pictures'), (req, res, next
         description: req.body.description,
         pictures: req.file ? req.file.path : '../images/div-home1.jpg',
         likes: req.body.likes,
-        price: req.body.price
+        price: req.body.price,
+        users: userId,
+        username: usernameOfAuthor,
+
     }
     Event.findByIdAndUpdate(eventId, editEvent, {new: true})
         .then(() => {
