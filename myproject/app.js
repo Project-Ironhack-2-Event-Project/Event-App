e6596912b78ac8341624078fc938eee7f648e487
Dok <pierredok@outlook.fr>
2023-06-16 14:10:09 +0200

    Last before presentation

--- myproject/app.js
+++ myproject/app.js
@@ -18,6 +18,8 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+
+
 const capitalize = require("./utils/capitalize");
 const projectName = "myproject";
 
@@ -33,6 +35,8 @@ app.use("/", require("./routes/index.routes"));
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/", require('./routes/event.routes'));
 app.use("/", require('./routes/privates.routes'));
+
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
